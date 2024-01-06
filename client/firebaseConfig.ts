9150ad396a18917e72c680cd7b1d012a445dfeca
Irfan Khan <irfan@infopower.co.nz>
2024-01-07 01:49:36 +1100

    feat: initial commit for image editor

---
+++ client/firebaseConfig.ts
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_LKN1nUOm2BZYe8QQJ8nbWRiiah3e1A4",
+  authDomain: "eyecastdev2videoeditor.firebaseapp.com",
+  projectId: "eyecastdev2videoeditor",
+  storageBucket: "eyecastdev2videoeditor.appspot.com",
+  messagingSenderId: "20519433409",
+  appId: "1:20519433409:web:c38ba09f49fb31c35b5040",
+};
+
+export default firebaseConfig;
